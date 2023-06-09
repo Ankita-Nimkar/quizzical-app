c0fef2830f41cf821aac24805c85aa12f50f0859
Ankita-Nimkar <106879213+Ankita-Nimkar@users.noreply.github.com>
2023-06-09 17:02:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
     let currentIndex = array.length,
       randomIndex;
 
-    while (currentIndex != 0) {
+    while (currentIndex !== 0) {
       // Pick a remaining element.
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
